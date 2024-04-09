5146505257ebda160e2aa6d04d3c2f824f86b6a9
SiyoungOh <81408668+siyoungoh@users.noreply.github.com>
2024-04-09 18:41:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 3000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
